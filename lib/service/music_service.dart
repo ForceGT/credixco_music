5b61b9ddb2042614eb2f629b6e6b4f2ba5662c0a
gtxtreme <thakkargaurav409@gmail.com>
2020-11-26 18:47:51 +0530

    Add internet utils to music_service.dart

--- lib/service/music_service.dart
+++ lib/service/music_service.dart
@@ -1,11 +1,14 @@
 import 'dart:convert';
 import 'package:credixco_music_match/model/Track.dart';
 import 'package:credixco_music_match/util/constants.dart';
-import 'package:flutter/material.dart';
+import 'package:connectivity/connectivity.dart';
 import 'package:http/http.dart' as http;
 
 class MusicService {
-  Stream getConnectivityStream() {}
+  Stream<ConnectivityResult> getConnectivityStream() {
+    Connectivity connectivity = new Connectivity();
+    return connectivity.onConnectivityChanged;
+  }
 
   Future<List<Track>> getAllTracks() async {
     var response = await http.get(Constants.TRACK_MASTER);
