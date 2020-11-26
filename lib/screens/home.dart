23515aa7cc59f56821bcb78674dd2198b3d12ded
gtxtreme <thakkargaurav409@gmail.com>
2020-11-26 20:39:51 +0530

    Change home.dart to show list of tracks

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,7 @@
+import 'package:credixco_music_match/bloc/music_cubit.dart';
+import 'package:credixco_music_match/widgets/music_item.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Home extends StatefulWidget {
 
@@ -6,13 +9,42 @@ class Home extends StatefulWidget {
   _HomeState createState() => _HomeState();
 }
 
+
 class _HomeState extends State<Home> {
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text("Wait for stuff to happen"),
+      body:
+      BlocBuilder<MusicCubit,MusicState>(
+        builder: (context,state){
+          debugPrint("State :$state");
+          if(state is MusicLoadingState){
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          else if(state is MusicSuccessState){
+            var trackList = state.trackList;
+            return ListView.builder(
+                itemCount: 10,
+                itemBuilder: (context,index){
+                  return MusicItem(trackName: trackList[index].trackName, trackAlbumName: trackList[index].albumName, trackSingerName: trackList[index].artistName,);
+                }
+            );
+          }
+          else if(state is MusicErrorState){
+            return Center(
+              child: Text("${state.error}"),
+            );
+          }
+          else{
+            return Container();
+          }
+
+        },
+
       ),
     );
   }
