896340542d2a696e4f46e9792ece0f0a37d26b10
gtxtreme <thakkargaurav409@gmail.com>
2020-11-26 16:58:26 +0530

    Add endpoints

---
+++ lib/util/constants.dart
@@ -0,0 +1,10 @@
+class Constants {
+  Constants._();
+
+  static String TRACK_MASTER =
+      "https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=2d782bc7a52a41ba2fc1ef05b9cf40d7";
+  static String TRACK_DETAIL =
+      "https://api.musixmatch.com/ws/1.1/track.get?apikey=2d782bc7a52a41ba2fc1ef05b9cf40d7&track_id=";
+  static String TRACK_LYRICS =
+      "https://api.musixmatch.com/ws/1.1/track.lyrics.get?apikey=2d782bc7a52a41ba2fc1ef05b9cf40d7&track_id=";
+}
