0cf2038dfc8d019b357ad115c5c0a733c776d23c
gtxtreme <thakkargaurav409@gmail.com>
2020-11-26 18:47:05 +0530

    Shift HomePage to home.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,86 +1,23 @@
+import 'package:credixco_music_match/screens/home.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         primarySwatch: Colors.blue,
-
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Home(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
 
 
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-
-
-
-
-
-    return FutureBuilder(
-      future: getTrackList(),
-      builder:(context, AsyncSnapshot<http.Response> snapshot){
-
-        if(snapshot.hasData){
-          var response = jsonDecode(snapshot.data.body);
-          print(response["message"]["body"]["track_list"].runtimeType);
-          return Scaffold(
-            body: Center(
-              child: Text("${response["message"]["body"]["track_list"]}"),
-            ),
-          );
-        }
-        else{
-          return Scaffold(
-            body: Center(
-              child: CircularProgressIndicator(),
-            ),
-          );
-        }
-
-      }
-
-
-    );
-  }
-
-
-  Future<http.Response> getTrackList() async{
-    var url = "https://api.musixmatch.com/ws/1.1/chart.tracks.get?apikey=2d782bc7a52a41ba2fc1ef05b9cf40d7";
-    return http.get(url);
-  }
-}
