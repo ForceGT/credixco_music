0b11cde990390af4260f4e956ee1600339e538f2
gtxtreme <thakkargaurav409@gmail.com>
2020-11-27 11:20:15 +0530

    Add support for userTap

--- lib/widgets/music_item.dart
+++ lib/widgets/music_item.dart
@@ -1,13 +1,16 @@
+import 'package:credixco_music_match/screens/track_detail.dart';
 import 'package:flutter/material.dart';
 
 class MusicItem extends StatelessWidget {
 
   final String trackName, trackAlbumName, trackSingerName;
-  MusicItem({this.trackAlbumName, this.trackName, this.trackSingerName});
+  final VoidCallback onTap;
+  MusicItem({this.trackAlbumName, this.trackName, this.trackSingerName,@required this.onTap});
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+      onTap:onTap,
       contentPadding: EdgeInsets.all(8.0),
       leading: Icon(Icons.library_music),
       title: Text("$trackName"),
